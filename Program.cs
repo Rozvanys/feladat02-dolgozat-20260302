9e926ee2af2b54e68acd1768ed0e9e292325180a
Salamon <rozvanys@gmail.com>
2026-03-02 13:41:09 +0100

    ameddig jutottam

--- Program.cs
+++ Program.cs
@@ -8,8 +8,34 @@ namespace feladat02
 {
 	internal class Program
 	{
+		static void jatek()
+		{
+			int[,] bingoTabla = new int[10, 5];
+
+			for (int i = 0; i < bingoTabla.GetLength(0); i++)
+			{
+				Console.WriteLine($"Kérem a(z) {i+1} játékos adjon meg 5 számot!");
+				for (int j = 0; j < bingoTabla.GetLength(1); j++)
+				{
+					bingoTabla[i, j] = Convert.ToInt32(Console.ReadLine());
+					while (bingoTabla[i, j] < 1 || bingoTabla[i, j] > 75)
+					{
+						Console.WriteLine("Hibás szám! Kérem adjon meg egy új számot!");
+						bingoTabla[i, j] = Convert.ToInt32(Console.ReadLine());
+					}
+				}
+			}
+			//for (int i = 0; i < bingoTabla.GetLength(0); i++)
+			//{
+			//	for (int j = 0; j < bingoTabla.GetLength(1); j++)
+			//	{
+			//		Console.WriteLine(bingoTabla[i, j]);
+			//	}
+			//}
+		}
 		static void Main(string[] args)
 		{
+			jatek();
 		}
 	}
 }
